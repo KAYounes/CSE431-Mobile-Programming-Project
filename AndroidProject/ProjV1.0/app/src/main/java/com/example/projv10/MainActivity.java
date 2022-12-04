1b03c543e29a443c6e19ac92cda628b233d88fd9
Kareem Amr Younes <official.kyounes@gmail.com>
2022-12-04 09:43:21 +0200

    Add view vinding to main activity

--- AndroidProject/ProjV1.0/app/src/main/java/com/example/projv10/MainActivity.java
+++ AndroidProject/ProjV1.0/app/src/main/java/com/example/projv10/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.projv10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import com.example.projv10.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        binder = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binder.getRoot();
+        setContentView(view);
     }
 }
