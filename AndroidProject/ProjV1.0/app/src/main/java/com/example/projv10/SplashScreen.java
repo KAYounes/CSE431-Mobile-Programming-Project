2876999813d0fb1c5279de09e69694e930557f3e
Kareem Amr Younes <official.kyounes@gmail.com>
2022-12-04 09:40:35 +0200

    Add splash screen fragment and use view binding

---
+++ AndroidProject/ProjV1.0/app/src/main/java/com/example/projv10/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.projv10;
+
+import android.os.Binder;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.projv10.databinding.FragmentSplashScreenBinding;
+
+public class SplashScreen extends Fragment {
+
+    private FragmentSplashScreenBinding binder;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binder = FragmentSplashScreenBinding.inflate(inflater, container, false);
+        View view = binder.getRoot();
+
+        return view; // inflater.inflate(R.layout.fragment_splash_screen, container, false);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binder = null;
+    }
+}
