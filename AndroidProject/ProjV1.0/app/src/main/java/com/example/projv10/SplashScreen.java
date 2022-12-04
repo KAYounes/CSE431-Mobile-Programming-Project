1b307941c634513850b80307a2138e8f12a3122a
Kareem Amr Younes <official.kyounes@gmail.com>
2022-12-04 11:04:25 +0200

    Add dynamic status bar color change

--- AndroidProject/ProjV1.0/app/src/main/java/com/example/projv10/SplashScreen.java
+++ AndroidProject/ProjV1.0/app/src/main/java/com/example/projv10/SplashScreen.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 import com.example.projv10.databinding.FragmentSplashScreenBinding;
 
@@ -19,6 +20,7 @@ public class SplashScreen extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binder = FragmentSplashScreenBinding.inflate(inflater, container, false);
         View view = binder.getRoot();
+        getActivity().getWindow().setStatusBarColor(getResources().getColor(R.color.palette_primary));
 
         return view; // inflater.inflate(R.layout.fragment_splash_screen, container, false);
     }
